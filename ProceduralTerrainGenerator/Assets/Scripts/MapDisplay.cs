e0e95a9080774978a654949b24d1b5fe8aa17d37
Brockosh <115765946+Brockosh@users.noreply.github.com>
2024-10-21 17:13:12 +1100

    Colour drawn onto mesh now.

--- ProceduralTerrainGenerator/Assets/Scripts/MapDisplay.cs
+++ ProceduralTerrainGenerator/Assets/Scripts/MapDisplay.cs
@@ -6,33 +6,12 @@ public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRenderer;
 
-    public void DrawNoiseMap(float[,] noiseMap)
+    public void DrawTexture(Texture2D texture)
     {
-        // Returns number of rows
-        int width = noiseMap.GetLength(0);
-        // Returns number of columns
-        int height = noiseMap.GetLength(1);
-
-
-        Texture2D texture = new Texture2D(width, height);
-
-        Color[] colorMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                // y * width gives us the index of the row we are on and adding x gives us column
-                // Noise map at the end applies a percentage between 0 and 1 between black and white
-                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-            }
-        }
-        texture.SetPixels(colorMap);
-        texture.Apply();
-
         // Shared materials allows us to look at map in editor so we don't have to be in play mode 
         textureRenderer.sharedMaterial.mainTexture = texture;
 
         // Sets size of plane to size of map
-        textureRenderer.transform.localScale = new Vector3(width, 1, height);
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
     }
 }
--- ProceduralTerrainGenerator/Assets/Scripts/Noise.cs
+++ ProceduralTerrainGenerator/Assets/Scripts/Noise.cs
@@ -5,27 +5,104 @@ using UnityEngine;
 public static class Noise
 {
 
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        //prng = pseudo random number generator
+        //Using this to track different maps
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for (int i = 0; i < octaves; i++)
+        {
+            //Don't want to give the perlin noise a value too high so we keep within a range
+            float offsetX = prng.Next(-100000, 1000000) + offset.x;
+            float offsetY = prng.Next(-100000, 1000000) + offset.y;
+
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (scale <= 0)
         {
             scale = 0.0001f;
         }
 
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        //Allow ourselves to zoom into the center instead of top right in editor
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight/ 2f;
+
+
+
         for (int y = 0; y < mapHeight; y++) 
         { 
             for (int x = 0; x < mapWidth; x++) 
             {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
+                //Think y axis for amplitude
+                float amplitude = 1;
+                //Think x axis for frequency
+                float frequency = 1;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+
+                //Want to keep track of our current height value
+                //This is so when we add octaves, the new value doesn't just reset the height but
+                //builds off the current height
+                float noiseHeight = 0;
+
+                //Loop through octaves
+                for (int i = 0; i < octaves; i++)
+                {
+                    // Divide x and y by scale to get some non integer values as perlin noise repeats at whole numbers
+                    //The higher the frequency, the further apart the sample points will be
+                    //Which will mean that the height values will change more rapidly
+                    float sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    //* 2 - 1 gives us perlin values that are occasionally in the negative to give us more variation in our land
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseMap[x, y] = perlinValue;
+
+                    noiseHeight += perlinValue * amplitude;
+
+                    //At the end of each octave
+                    //Persistance is 0 - 1 range so it decreases the amplitude
+                    amplitude *= persistance;
+
+                    //Frequency increases each octave as the lacunarity should be greater than one
+                    frequency *= lacunarity;
+                
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                //Apply the noiseHeight
+                noiseMap[x, y] = noiseHeight;
             }
         }
-        return noiseMap;
+
+        //Normalise noise map
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                // Inverse lerp gives us a value between 0 and 1, so if the value is above or below zero
+                // it will snap them back to 1 or 0
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
+
+                return noiseMap;
     }
 
 
