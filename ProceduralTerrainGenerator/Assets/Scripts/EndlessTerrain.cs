a00eda786de7095d6c337c2648513fe651094e13
Brockosh <115765946+Brockosh@users.noreply.github.com>
2024-11-11 15:19:08 +1100

    Half way through second refactor.

--- ProceduralTerrainGenerator/Assets/Scripts/EndlessTerrain.cs
+++ ProceduralTerrainGenerator/Assets/Scripts/EndlessTerrain.cs
@@ -89,207 +89,23 @@ public class EndlessTerrain : MonoBehaviour
         }
     }
 
-    public class TerrainChunk
+   
+
+
+}
+
+[System.Serializable]
+public struct LODInfo
+{
+    [Range(0, MeshSettings.numberOfSupportedLODs - 1)]
+    public int lod;
+    public float visibleDstThreshold;
+
+    public float sqrVisibleDstThreshold
     {
-        public Vector2 coord;
-
-        GameObject meshObject;
-        Vector2 sampleCentre;
-        Bounds bounds;
-
-        MeshRenderer meshRenderer;
-        MeshFilter meshFilter;
-        MeshCollider meshCollider;
-
-        LODInfo[] detailLevels;
-        LODMesh[] lodMeshes;
-        int colliderLODIndex;
-
-        HeightMap mapData;
-        bool mapDataReceived;
-        int previousLODIndex = -1;
-        bool hasSetCollider;
-
-        public TerrainChunk(Vector2 coord, float meshWorldSize, LODInfo[] detailLevels, int colliderLODIndex, Transform parent, Material material)
+        get
         {
-            this.coord = coord;
-            this.detailLevels = detailLevels;
-            this.colliderLODIndex = colliderLODIndex;
-
-            sampleCentre = coord * meshWorldSize / mapGenerator.meshSettings.meshScale;
-            Vector2 position = coord * meshWorldSize;
-            bounds = new Bounds(position, Vector2.one * meshWorldSize);
-            
-
-            meshObject = new GameObject("Terrain Chunk");
-            meshRenderer = meshObject.AddComponent<MeshRenderer>();
-            meshFilter = meshObject.AddComponent<MeshFilter>();
-            meshCollider = meshObject.AddComponent<MeshCollider>();
-            meshRenderer.material = material;
-
-            meshObject.transform.position = new Vector3(position.x, 0, position.y);
-            meshObject.transform.parent = parent;
-            SetVisible(false);
-
-            lodMeshes = new LODMesh[detailLevels.Length];
-
-            for (int i = 0; i < detailLevels.Length; i++)
-            {
-                lodMeshes[i] = new LODMesh(detailLevels[i].lod);
-                lodMeshes[i].updateCallback += UpdateTerrainChunk;
-                if (i == colliderLODIndex)
-                {
-                    lodMeshes[i].updateCallback += UpdateCollisionMesh;
-                }
-            }
-
-            mapGenerator.RequestHeightMap(sampleCentre, OnMapDataReceived);
-        }
-
-        void OnMapDataReceived(HeightMap mapData)
-        {
-            this.mapData = mapData;
-            mapDataReceived = true;
-
-            UpdateTerrainChunk();
-        }
-
-        public void UpdateTerrainChunk()
-        {
-            if (mapDataReceived)
-            {
-                float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
-
-                bool wasVisible = IsVisible();
-                bool visible = viewerDstFromNearestEdge <= maxViewDst;
-
-                if (visible)
-                {
-                    int lodIndex = 0;
-
-                    for (int i = 0; i < detailLevels.Length - 1; i++)
-                    {
-                        if (viewerDstFromNearestEdge > detailLevels[i].visibleDstThreshold)
-                        {
-                            lodIndex = i + 1;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    if (lodIndex != previousLODIndex)
-                    {
-                        LODMesh lodMesh = lodMeshes[lodIndex];
-                        if (lodMesh.hasMesh)
-                        {
-                            previousLODIndex = lodIndex;
-                            meshFilter.mesh = lodMesh.mesh;
-                        }
-                        else if (!lodMesh.hasRequestedMesh)
-                        {
-                            lodMesh.RequestMesh(mapData);
-                        }
-                    }
-
-
-                    visibleTerrainChunks.Add(this);
-                }
-
-
-                if (wasVisible != visible)
-                {
-                    if (visible)
-                    {
-                        visibleTerrainChunks.Add(this);
-                    }
-                    else
-                    {
-                        visibleTerrainChunks.Remove(this);
-                    }
-                    SetVisible(visible);
-                }
-            }
-        }
-
-        public void UpdateCollisionMesh()
-        {
-            if (!hasSetCollider)
-            {
-                float sqrDstFromViewerToEdge = bounds.SqrDistance(viewerPosition);
-
-                if (sqrDstFromViewerToEdge < detailLevels[colliderLODIndex].sqrVisibleDstThreshold)
-                {
-                    if (!lodMeshes[colliderLODIndex].hasRequestedMesh)
-                    {
-                        lodMeshes[colliderLODIndex].RequestMesh(mapData);
-                    }
-                }
-
-                if (sqrDstFromViewerToEdge < colliderGenerationDistanceThreshold * colliderGenerationDistanceThreshold)
-                {
-                    if (lodMeshes[colliderLODIndex].hasMesh)
-                    {
-                        meshCollider.sharedMesh = lodMeshes[colliderLODIndex].mesh;
-                        hasSetCollider = true;
-                    }
-                }
-            }
-        }
-
-        public void SetVisible(bool visible)
-        {
-            meshObject.SetActive(visible);
-        }
-
-        public bool IsVisible()
-        {
-            return meshObject.activeSelf;
+            return visibleDstThreshold * visibleDstThreshold;
         }
     }
-
-    class LODMesh
-    {
-        public Mesh mesh;
-        public bool hasRequestedMesh;
-        public bool hasMesh;
-        int lod;
-        public event System.Action updateCallback;
-
-        public LODMesh(int lod)
-        {
-            this.lod = lod;
-        }
-
-        void OnMeshDataReceived(MeshData meshData)
-        {
-            mesh = meshData.CreateMesh();
-            hasMesh = true;
-
-            updateCallback();
-        }
-
-        public void RequestMesh(HeightMap mapData)
-        {
-            hasRequestedMesh = true;
-            mapGenerator.RequestMeshData(mapData, lod, OnMeshDataReceived);
-        }
-    }
-
-    [System.Serializable]
-    public struct LODInfo
-    {
-        [Range(0, MeshSettings.numberOfSupportedLODs - 1)]
-        public int lod;
-        public float visibleDstThreshold;
-
-        public float sqrVisibleDstThreshold
-        {
-            get
-            {
-                return visibleDstThreshold * visibleDstThreshold;
-            }
-        }
-    }
-
 }
--- ProceduralTerrainGenerator/Assets/Scripts/MapGenerator.cs
+++ ProceduralTerrainGenerator/Assets/Scripts/MapGenerator.cs
@@ -27,9 +27,6 @@ public class MapGenerator : MonoBehaviour
 
     float[,] falloffMap;
 
-    Queue<MapThreadInfo<HeightMap>> heightMapThreadInfoQueue = new Queue<MapThreadInfo<HeightMap>>();
-    Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
-
 
     private void Start()
     {
@@ -72,66 +69,6 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-    public void RequestHeightMap(Vector2 centre, Action<HeightMap> callback)
-    {
-        ThreadStart threadStart = delegate
-        {
-            HeightMapThread(centre, callback);
-        };
-
-        new Thread(threadStart).Start();
-    }
-
-    void HeightMapThread(Vector2 centre, Action<HeightMap> callback)
-    {
-        HeightMap heightMap = HeightMapGenerator.GenerateHeightMap(meshSettings.numVertsPerLine, meshSettings.numVertsPerLine, heightMapSettings, centre);
-        lock (heightMapThreadInfoQueue)
-        {
-            heightMapThreadInfoQueue.Enqueue(new MapThreadInfo<HeightMap>(callback, heightMap));
-        }
-    }
-
-    public void RequestMeshData(HeightMap heightMap, int lod, Action<MeshData> callback)
-    {
-        ThreadStart threadStart = delegate
-        {
-            MeshDataThread(heightMap, lod, callback);
-        };
-
-        new Thread(threadStart).Start();
-    }
-
-    void MeshDataThread(HeightMap heightMap, int lod, Action<MeshData> callback)
-    {
-        MeshData meshData = MeshGenerator.GenerateTerrainMesh(heightMap.values, meshSettings, lod);
-        lock (meshDataThreadInfoQueue)
-        {
-            meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
-        }
-    }
-
-
-    private void Update()
-    {
-        if (heightMapThreadInfoQueue.Count > 0)
-        {
-            for (int i = 0; i < heightMapThreadInfoQueue.Count; i++)
-            {
-                MapThreadInfo<HeightMap> threadInfo = heightMapThreadInfoQueue.Dequeue();
-                threadInfo.callback(threadInfo.parameter);
-            }
-        }
-
-        if (meshDataThreadInfoQueue.Count > 0)
-        {
-            for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
-            {
-                MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
-                threadInfo.callback(threadInfo.parameter);
-            }
-        }
-    }
-
     private void OnValidate()
     {
 
@@ -153,18 +90,7 @@ public class MapGenerator : MonoBehaviour
 
     }
 
-    struct MapThreadInfo<T>
-    {
-        public readonly Action<T> callback;
-        public readonly T parameter;
-
-        public MapThreadInfo(Action<T> callback, T parameter) 
-        { 
-            this.callback = callback;   
-            this.parameter = parameter;
-        }
-
-    }
+   
 }
 
 
